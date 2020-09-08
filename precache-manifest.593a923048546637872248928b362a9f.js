da32bec91a2070d496b363b2fbff800df58c128d
developer@whizgen.in <rileymanda1@gmail.com>
2020-09-08 23:02:59 +0530

    Updates

---
+++ precache-manifest.593a923048546637872248928b362a9f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d4ab4afcac4c15b6e2c70cd0d76203",
+    "url": "/pdfapp_deploy/index.html"
+  },
+  {
+    "revision": "cac8f4affc66526ae920",
+    "url": "/pdfapp_deploy/static/css/2.57314b1f.chunk.css"
+  },
+  {
+    "revision": "9fef0b53ce8c965ac964",
+    "url": "/pdfapp_deploy/static/css/main.fa9cf203.chunk.css"
+  },
+  {
+    "revision": "cac8f4affc66526ae920",
+    "url": "/pdfapp_deploy/static/js/2.810b51e6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pdfapp_deploy/static/js/2.810b51e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fef0b53ce8c965ac964",
+    "url": "/pdfapp_deploy/static/js/main.15e7b3f2.chunk.js"
+  },
+  {
+    "revision": "a2c2f89964528d790064",
+    "url": "/pdfapp_deploy/static/js/runtime-main.c5264075.js"
+  },
+  {
+    "revision": "d4e82fdd8ffd0181ee316255e857e7d0",
+    "url": "/pdfapp_deploy/static/media/banner-bottom-shape.d4e82fdd.png"
+  },
+  {
+    "revision": "e06317d76ba7d8d5c518dbeac9b2dd50",
+    "url": "/pdfapp_deploy/static/media/bg.e06317d7.jpg"
+  },
+  {
+    "revision": "9c27def6db2de1f02fc9391c1764a032",
+    "url": "/pdfapp_deploy/static/media/contact-bg.9c27def6.png"
+  },
+  {
+    "revision": "402ae8b3da34b892e3f0f9a87c8657ef",
+    "url": "/pdfapp_deploy/static/media/page-header.402ae8b3.jpg"
+  }
+]);
